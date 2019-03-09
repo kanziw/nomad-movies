b1581098071f5e5f71de0d4ced9f91bf1df3f88e
kanziw <kanziwoong@gmail.com>
2019-03-09 13:58:22 +0900

    Load asset
    expo login
    yarn star

--- App.js
+++ App.js
@@ -1,14 +1,34 @@
+import { Ionicons } from '@expo/vector-icons'
+import { AppLoading, Asset, Font } from 'expo'
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import kanziw from './assets/kanziw.png'
 
 export default class App extends React.Component {
-  render() {
-    return (
+  state = { loaded: false }
+
+  handleError = error => console.log(error)
+
+  handleLoaded = () => this.setState({ loaded: true })
+
+  loadAssets = async () => {
+    await Font.loadAsync({ ...Ionicons.font })
+    await Asset.loadAsync([ kanziw ])
+  }
+
+  render = () => this.state.loaded
+    ? (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
       </View>
-    )
-  }
+    ) :
+    <AppLoading
+      startAsync={this.loadAssets}
+      onFinish={this.handleLoaded}
+      onError={this.handleError}
+    />
+
+
 }
 
 const styles = StyleSheet.create({
