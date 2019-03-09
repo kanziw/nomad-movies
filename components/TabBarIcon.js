74fcada98982555b3606bf7042c40cb953cb839f
kanziw <kanziwoong@gmail.com>
2019-03-09 19:49:16 +0900

    Add icon on tab bar navigation

---
+++ components/TabBarIcon.js
@@ -0,0 +1,18 @@
+import { Ionicons } from '@expo/vector-icons'
+import PropTyes from 'prop-types'
+import React from 'react'
+import { ACTIVE_COLOR, INACTIVE_COLOR } from '../constants/Color'
+
+const TabBarIcon = ({ name, focused }) =>
+  <Ionicons
+    size={26}
+    name={name}
+    color={focused ? ACTIVE_COLOR : INACTIVE_COLOR}
+  />
+
+TabBarIcon.propTypes = {
+  name: PropTyes.string.isRequired,
+  focused: PropTyes.bool.isRequired,
+}
+
+export default TabBarIcon
--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -1,14 +1,49 @@
-import { BG_COLOR } from '../constants/Color'
+import React from 'react'
 import { createAppContainer, createBottomTabNavigator } from 'react-navigation'
+import {Platform} from 'react-native'
+import TabBarIcon from '../components/TabBarIcon'
+import { BG_COLOR } from '../constants/Color'
 import MovieScreen from '../screens/Movies'
 import SearchScreen from '../screens/Search'
 import TVScreen from '../screens/TV'
 
+const ICON_PREFIX = Platform.OS === 'ios' ? 'ios' : 'md'
+
 const TabNavigation = createBottomTabNavigator(
   {
-    Movies: MovieScreen,
-    TV: TVScreen,
-    Search: SearchScreen,
+    Movie: {
+      screen: MovieScreen,
+      navigationOptions: {
+        tabBarIcon: ({ focused }) => (
+          <TabBarIcon
+            focused={focused}
+            name={`${ICON_PREFIX}-film`}
+          />
+        )
+      }
+    },
+    TV: {
+      screen: TVScreen,
+      navigationOptions: {
+        tabBarIcon: ({ focused }) => (
+          <TabBarIcon
+            focused={focused}
+            name={`${ICON_PREFIX}-tv`}
+          />
+        )
+      }
+    },
+    Search: {
+      screen: SearchScreen,
+      navigationOptions: {
+        tabBarIcon: ({ focused }) => (
+          <TabBarIcon
+            focused={focused}
+            name={`${ICON_PREFIX}-search`}
+          />
+        )
+      }
+    },
   },
   {
     tabBarOptions: {
