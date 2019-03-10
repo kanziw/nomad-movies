05fa712adda3dfd15368edf3f85669f787fdbdb1
kanziw <kanziwoong@gmail.com>
2019-03-10 23:08:27 +0900

    Fill MovieSlide content and styling

---
+++ components/MovieRating.js
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import styled from 'styled-components'
+import { GREY_COLOR, TINT_COLOR } from '../constants/Color'
+
+const Vote = styled.Text`
+  color: ${props => props.inSlide ? TINT_COLOR : GREY_COLOR};
+  font-size: ${props => props.inSlide ? '10px' : '8px'};
+  font-weight: 600;
+`
+
+const MovieRating = ({ votes, inSlide = false }) => (
+  <Vote inSlide={inSlide}>⭐️ {`${votes} / 10`}</Vote>
+)
+
+MovieRating.propTypes = {
+  votes: PropTypes.number.isRequired,
+  inSlide: PropTypes.bool,
+}
+
+export default MovieRating
--- components/MovieSlide.js
+++ components/MovieSlide.js
@@ -1,18 +1,21 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 import styled from 'styled-components'
+import MovieRating from '../components/MovieRating'
+import { TINT_COLOR } from '../constants/Color'
 import Layout from '../constants/Layout'
 import makePhotoUrl from '../utils/makePhotoUrl'
 import MoviePoster from './MoviePoster'
 
 const Container = styled.View`
   flex: 1;
+  position: relative;
 `
 
 const BgImage = styled.Image`
   width: ${Layout.width};
   height: ${Layout.height / 3};
-  opacity: 0.5;
+  opacity: 0.3;
   position: absolute;
 `
 
@@ -22,6 +25,38 @@ const Content = styled.View`
   align-items: center;
   padding-horizontal: 30px;
   justify-content: space-between;
+`
+
+const Column = styled.View`
+  width: 60%;
+  align-items: flex-start;
+`
+
+const Title = styled.Text`
+  color: ${TINT_COLOR};
+  font-size: 14px;
+  font-weight: 600;
+`
+
+const Overview = styled.Text`
+  color: ${TINT_COLOR};
+  font-size: 12px;
+  margin-bottom: 10px;
+`
+
+const VoteContainer = styled.View`
+  margin: 10px 0;
+`
+
+const BtnContainer = styled.TouchableOpacity`
+  background-color: #e74c3c;
+  padding: 5px;
+  border-radius: 2.5px;
+`
+
+const BtnText = styled.Text`
+  color: white;
+  font-size: 12px;
 `
 
 const MovieSlide = (
@@ -38,6 +73,24 @@ const MovieSlide = (
     <BgImage source={{ uri: makePhotoUrl(backgroundPhoto) }} />
     <Content>
       <MoviePoster path={posterPhoto} />
+      <Column>
+        <Title>{title}</Title>
+        {voteAvg ? (
+          <VoteContainer>
+            <MovieRating votes={voteAvg} inSlide={true} />
+          </VoteContainer>
+        ) : null}
+        {overview ? (
+          <Overview>
+            {overview.length > 117
+              ? `${overview.substring(0, 120)}...`
+              : overview}
+          </Overview>
+        ) : null}
+        <BtnContainer>
+          <BtnText>More details</BtnText>
+        </BtnContainer>
+      </Column>
     </Content>
   </Container>
 )
